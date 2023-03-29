c1831805cd0e00c5e67d106efb39484c4fd3100f
Umair Farooqui <info.mufazmi@gmail.com>
2023-03-29 17:07:25 +0530

    Implemnted : Error Hanlder & Middleware

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+
+class Constants{
+ 
+     static SERVER_MESSAGE = {
+        NOT_FOUND : "Oops..! It's 404",
+        FORBIDDEN : "Oops..! It's 403",
+        BAD_REQUEST : "Bad Request",
+        SERVER_ERROR : "Oops..! Something went wrong"
+     }
+
+     static STATUS_CODE = {
+        NOT_FOUND : 404,
+        FORBIDDEN:403,
+        BAD_REQUEST:400,
+        SERVER_ERROR:500
+     }
+
+}
+
+export default Constants
