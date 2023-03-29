4e9be739147e5438107909ff726f294a81ec4fc2
Umair Farooqui <info.mufazmi@gmail.com>
2023-03-30 01:21:48 +0530

    removed pvt info

--- src/services/sms-service.ts
+++ src/services/sms-service.ts
@@ -28,7 +28,9 @@ class SmsService {
 
     sendSms = async (data: iSendSms) => {
         try {
-            const url = `http://182.18.143.11/api/mt/SendSMS?apikey=xzj3uC95O0qk5MxB6tO1Gg&senderid=EGPAID&channel=TRANS&DCS=0&flashs ms=0&number=${data.mobile}&text=${data.text}&route=15&DLTTemplateId=1207161596306313471`;
+            const apiKey = 'test'
+            const domain = 'ab.com'
+            const url = `${domain}/api/mt/SendSMS?apikey=${apiKey}&senderid=test&channel=test&DCS=0&flashs ms=0&number=${data.mobile}&text=${data.text}&route=15&DLTTemplateId=1207161596306313471`;
             const res = await axios.get(url);
             console.log(res);
         }
