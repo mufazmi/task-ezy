d35cd0a810d73f3c47104cfdedd76b6a4fd48265
Umair Farooqui <34832943+mufazmi@users.noreply.github.com>
2023-03-31 12:00:52 +0000

    Bug Fixes : Changed Undefined -> DB_PASSWORD to DB_PASS

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -6,7 +6,7 @@ const production = {
     PORT:process.env.PORT,
     DB_HOST:process.env.DB_HOST,
     DB_USER:process.env.DB_USER,
-    DB_PASSWORD:process.env.DB_PASSWORD,
+    DB_PASS:process.env.DB_PASS,
     DB_NAME:process.env.DB_NAME,
     DB_DIALECT:process.env.DB_DIALECT,
 }
@@ -17,7 +17,7 @@ const staging = {
     PORT:process.env.PORT,
     DB_HOST:process.env.DB_HOST,
     DB_USER:process.env.DB_USER,
-    DB_PASSWORD:process.env.DB_PASSWORD,
+    DB_PASS:process.env.DB_PASS,
     DB_NAME:process.env.DB_NAME,
     DB_DIALECT:process.env.DB_DIALECT,
 }
@@ -28,7 +28,7 @@ const development = {
     PORT:process.env.PORT,
     DB_HOST:process.env.DB_HOST,
     DB_USER:process.env.DB_USER,
-    DB_PASSWORD:process.env.DB_PASSWORD,
+    DB_PASS:process.env.DB_PASS,
     DB_NAME:process.env.DB_NAME,
     DB_DIALECT:process.env.DB_DIALECT,
 }
